d5aec743291851030aedb4d82e3a21e60f5a12ef
Miraj <miraj.samarakkody@gmail.com>
2025-07-09 08:18:04 -0500

    up 0709 0817h

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,34 +1,32 @@
--- nvim-autopairs with cmp integration
 return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
+  "windwp/nvim-autopairs",
+  event = "InsertEnter",
+  config = function()
     local autopairs = require("nvim-autopairs")
+    local Rule = require("nvim-autopairs.rule")
+
     autopairs.setup({
-        check_ts = true, -- enable Treesitter integration
+      check_ts = true, -- enable Treesitter integration
     })
 
-    -- Optional: integration with nvim-cmp
+    -- Add custom rules for LaTeX environments
+    autopairs.add_rules {
+      Rule("\\(", "\\)", { "tex", "plaintex", "markdown" })
+        :with_pair(function(opts)
+          return opts.col >= 2 and opts.line:sub(opts.col - 1, opts.col - 1) == "\\"
+        end),
+      Rule("\\[", "\\]", { "tex", "plaintex", "markdown" })
+        :with_pair(function(opts)
+          return opts.col >= 2 and opts.line:sub(opts.col - 1, opts.col - 1) == "\\"
+        end),
+    }
+
+    -- Optional: cmp integration
     local cmp_status, cmp = pcall(require, "cmp")
     if cmp_status then
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-        end
-        end,
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end
+  end,
 }
 
--- Add custom rules
-local npairs = require("nvim-autopairs")
-local Rule   = require("nvim-autopairs.rule")
-
-npairs.add_rules {
-  Rule("\\(", "\\)", { "tex", "plaintex", "markdown" })
-    :with_pair(function(opts)
-      return opts.line:sub(opts.col - 1, opts.col - 1) == "\\"
-    end),
-
-  Rule("\\[", "\\]", { "tex", "plaintex", "markdown" })
-    :with_pair(function(opts)
-      return opts.line:sub(opts.col - 1, opts.col - 1) == "\\"
-    end),
-}
